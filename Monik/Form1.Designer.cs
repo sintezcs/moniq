1c111adca7129fd68728d0a6d79d2da5be74c7aa
Alexey Minakov <sintez.pm@gmail.com>
2015-05-11 20:13:34 +0300

    initial

---
+++ Monik/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Monik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fsWatcher = new System.IO.FileSystemWatcher();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstDirectories = new System.Windows.Forms.ListBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dirsListView = new System.Windows.Forms.ListView();
+            this.columnHeaderPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).BeginInit();
+            this.contextMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fsWatcher
+            // 
+            this.fsWatcher.EnableRaisingEvents = true;
+            this.fsWatcher.SynchronizingObject = this;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingMenuItem,
+            this.toolStripSeparator1,
+            this.exitMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(117, 54);
+            // 
+            // settingMenuItem
+            // 
+            this.settingMenuItem.Name = "settingMenuItem";
+            this.settingMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingMenuItem.Text = "Settings";
+            this.settingMenuItem.Click += new System.EventHandler(this.settingMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // lstDirectories
+            // 
+            this.lstDirectories.FormattingEnabled = true;
+            this.lstDirectories.Location = new System.Drawing.Point(12, 12);
+            this.lstDirectories.MultiColumn = true;
+            this.lstDirectories.Name = "lstDirectories";
+            this.lstDirectories.Size = new System.Drawing.Size(338, 121);
+            this.lstDirectories.TabIndex = 1;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(177, 139);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(81, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add path";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // btRemove
+            // 
+            this.btRemove.Location = new System.Drawing.Point(264, 139);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(86, 23);
+            this.btRemove.TabIndex = 3;
+            this.btRemove.Text = "Remove path";
+            this.btRemove.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 141);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MB - alarm dir size";
+            // 
+            // dirsListView
+            // 
+            this.dirsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderPath,
+            this.columnHeaderSize});
+            this.dirsListView.Location = new System.Drawing.Point(374, 12);
+            this.dirsListView.Name = "dirsListView";
+            this.dirsListView.Size = new System.Drawing.Size(329, 121);
+            this.dirsListView.TabIndex = 6;
+            this.dirsListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeaderPath
+            // 
+            this.columnHeaderPath.Text = "Path";
+            // 
+            // columnHeaderSize
+            // 
+            this.columnHeaderSize.Text = "Size";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 171);
+            this.Controls.Add(this.dirsListView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.lstDirectories);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monik";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.fsWatcher)).EndInit();
+            this.contextMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fsWatcher;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btRemove;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.ListBox lstDirectories;
+        private System.Windows.Forms.ToolStripMenuItem settingMenuItem;
+        private System.Windows.Forms.ListView dirsListView;
+        private System.Windows.Forms.ColumnHeader columnHeaderPath;
+        private System.Windows.Forms.ColumnHeader columnHeaderSize;
+    }
+}
+
---
+++ Monik/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monik
+{
+    public partial class Form1 : Form
+    {
+        protected List<MonitoredDir> MonitoredDirs = new List<MonitoredDir>(); 
+        
+        public Form1()
+        {
+            InitializeComponent();
+            var tmp = new MonitoredDir() { Path = Path.GetTempPath() };
+            MonitoredDirs.Add(tmp);
+            RefreshDirs();
+            dirsListView.Items.Add(new ListViewItem() { Name = tmp.Path, SubItems = { tmp.Path, tmp.Size.ToString() } });
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }            
+        }
+
+        private void settingMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void RefreshDirs()
+        {
+            foreach (var monitoredDir in MonitoredDirs)
+            {
+                UpdateDirInfo(monitoredDir);
+            }
+        }
+
+        private void UpdateDirInfo(MonitoredDir dir)
+        {
+            dir.Size = Math.Round((double)(GetDirectorySize(dir.Path) / 1000000), 2);
+        }
+
+        public static long GetDirectorySize(string parentDirectory)
+        {
+            return new DirectoryInfo(parentDirectory).GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
+        }
+    }
+
+    public class MonitoredDir
+    {
+        public string Path { get; set; }
+
+        public MonitoredDir()
+        {
+            Size = 0;
+        }
+
+        public double Size { get; set; }
+    }
+}
